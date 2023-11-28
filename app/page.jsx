00114c05b45e98f30d5eaec16a306cb200cba25b
taeyeon <hahaha2662@naver.com>
2023-11-28 12:17:46 +0900

    2

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,6 @@ export default function Home() {
       <h1 className="text-3xl font-bold">Recent News</h1>
       <p>MongoDB CRUD example</p>
       <p>Next-auth 인증</p>
-      <TopicList />
     </div>
   )
 }
